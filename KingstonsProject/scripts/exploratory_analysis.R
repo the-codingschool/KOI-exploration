4e45ab0919249ccfbee38f20a781f6aaa8fc3163
26kl0145 <kingstonli2026@gmail.com>
2022-07-22 16:12:27 -0400

    Adding exploratory analysis file and plots

---
+++ KingstonsProject/scripts/exploratory_analysis.R
@@ -0,0 +1,25 @@
+# Load required libraries
+library(dplyr)
+library(ggplot2)
+
+# Load dataset
+koi_data <- read.csv("data/cumulative_koi_data.csv")
+View(koi_data)
+
+
+
+#name_koi_data <- arrange(koi_data, desc(kepoi_name))
+#View(name_koi_data)
+
+ggplot(koi_data, aes(x = koi_pdisposition, y = koi_score, color = koi_disposition)) +
+  geom_boxplot()
+# There is 1 KOI with a Kepler data disposition of CANDIDATE but an archive disposition of FALSE POSITIVE
+# There are also a few KOIs with a Kepler data disposition of FALSE POSITIVE but an archive disposition of CONFIRMED
+
+
+ggplot(na_srad_koi_data, aes(x = koi_prad, y = koi_srad)) +
+  geom_point()
+
+cor(na_srad_koi_data$koi_prad, na_srad_koi_data$koi_srad)
+
+
