7c31648299728cf42a65f530317ac10bb40b4210
26kl0145 <kingstonli2026@gmail.com>
2022-07-22 16:11:58 -0400

    Loading and filtering dataset

--- KingstonsProject/scripts/cleaning_and_processing.R
+++ KingstonsProject/scripts/cleaning_and_processing.R
@@ -5,4 +5,15 @@ library(dplyr)
 library(ggplot2)
 
 # Load dataset
-koi_data <- read.csv()
+koi_data <- read.csv("data/cumulative_koi_data.csv")
+View(koi_data)
+
+
+
+
+na_srad_koi_data <- filter(koi_data, !is.na(koi_srad))
+View(na_srad_koi_data)
+
+confirmed_koi <- filter(koi_data, koi_disposition == "CONFIRMED")
+confirmed_koi2 <- filter(confirmed_koi, koi_pdisposition != "FALSE POSITIVE")
+View(confirmed_koi2)
