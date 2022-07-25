b9b819e2f02bf1ba48dbcc90bc3ded9aa9b981c9
26kl0145 <kingstonli2026@gmail.com>
2022-07-25 17:06:26 -0400

    Adding plots on transit duration across different dispositions and scores

--- KingstonsProject/scripts/exploratory_analysis.R
+++ KingstonsProject/scripts/exploratory_analysis.R
@@ -17,17 +17,55 @@ ggplot(koi_data, aes(x = koi_pdisposition, y = koi_score, color = koi_dispositio
 # There are also a few KOIs with a Kepler data disposition of FALSE POSITIVE but an archive disposition of CONFIRMED
 
 
-ggplot(na_srad_koi_data, aes(x = koi_prad, y = koi_srad)) +
-  geom_point()
+ggplot(na_srad_koi_data, aes(x = koi_prad, y = koi_srad, color = koi_disposition)) +
+  geom_point() +
+  labs(x = "Planetary radius",
+       y = "Stellar radius")
 
 cor(na_srad_koi_data$koi_prad, na_srad_koi_data$koi_srad)
 
 
 
+
 ggplot(candidate_koi, aes(x = koi_score, y = koi_duration)) +
-  geom_bar(stat = "summary",
-           fun = "mean") +
+  geom_point(stat = "summary",
+           fun = "mean",
+           color = "blue") +
   labs(title = 'Average transit duration of "CANDIDATE" KOIs by disposition score',
        x = "Disposition score",
        y = "Transit duration")
+ggplot(candidate_koi, aes(x = koi_duration)) +
+  geom_histogram()
 
+ggplot(confirmed_koi2, aes(x = koi_score, y = koi_duration)) +
+  geom_bar(stat = "summary",
+           fun = "mean",
+           fill = "springgreen3") +
+  labs(title = 'Average transit duration of "CONFIRMED" KOIs by disposition score',
+       x = "Disposition score",
+       y = "Transit duration")
+ggplot(confirmed_koi2, aes(x = koi_duration)) +
+  geom_histogram()
+
+ggplot(false_koi2, aes(x = koi_score, y = koi_duration)) +
+  geom_bar(stat = "summary",
+           fun = "mean",
+           fill = "red") +
+  labs(title = 'Average transit duration of "FALSE POSITIVE" KOIs by disposition score',
+       x = "Disposition score",
+       y = "Transit duration")
+ggplot(false_koi2, aes(x = koi_duration)) +
+  geom_histogram()
+
+
+ggplot(koi_data, aes(x = koi_duration)) +
+  geom_histogram(aes(fill = koi_disposition)) +
+  labs(x = "Transit duration (hours)")
+
+
+
+
+ggplot(koi_data, aes(x = koi_disposition, y = koi_duration)) +
+  geom_histogram(stat = "summary",
+                 fun = "mean",
+                 fill = "springgreen4")
