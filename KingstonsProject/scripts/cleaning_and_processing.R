e305daeb87b349746aa41e40d5eca1e8ca27afd9
26kl0145 <kingstonli2026@gmail.com>
2022-07-27 16:08:39 -0400

    Filtered disposition datasets to not have NA values and arranged overall dataset by disposition

--- KingstonsProject/scripts/cleaning_and_processing.R
+++ KingstonsProject/scripts/cleaning_and_processing.R
@@ -19,15 +19,18 @@ na_koi_data <- filter(clean_koi_data, !is.na(koi_srad))
 
 
 ## Filtering dataset into KOI dispositions of "CONFIRMED", "CANDIDATE", and "FALSE POSITIVE"s only
-### There is 1 KOI with a Kepler data disposition of CANDIDATE but an archive disposition of FALSE POSITIVE
-### There are also a few KOIs with a Kepler data disposition of FALSE POSITIVE but an archive disposition of CONFIRMED
+# There is 1 KOI with a Kepler data disposition of CANDIDATE but an archive disposition of FALSE POSITIVE
+# There are also a few KOIs with a Kepler data disposition of FALSE POSITIVE but an archive disposition of CONFIRMED
 confirmed_koi <- filter(clean_koi_data, koi_disposition == "CONFIRMED")
 confirmed_koi2 <- filter(confirmed_koi, koi_pdisposition != "FALSE POSITIVE")
+na_confirmed_koi <- filter(confirmed_koi2, !is.na(koi_srad))
 
 candidate_koi <- filter(clean_koi_data, koi_disposition == "CANDIDATE")
+na_candidate_koi <- filter(candidate_koi, !is.na(koi_srad))
 
 false_koi <- filter(clean_koi_data, koi_disposition == "FALSE POSITIVE")
 false_koi2 <- filter (false_koi, koi_pdisposition != "CANDIDATE")
+na_false_koi <- filter(false_koi2, !is.na(koi_srad))
 
-
-
+## Arranging na_koi_data by koi_disposition
+sorted_na_koi_data <-  arrange(na_koi_data, koi_disposition)
