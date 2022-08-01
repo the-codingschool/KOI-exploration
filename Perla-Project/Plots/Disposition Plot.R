fc0dc957171538ee8c0dbc2b15f645aa3c446d8a
Perla Lopez <perla23lopez@Perlas-MBP.attlocal.net>
2022-08-01 10:10:12 -0700

    Updating disposition plot file

--- Perla-Project/Plots/Disposition Plot.R
+++ Perla-Project/Plots/Disposition Plot.R
@@ -3,14 +3,6 @@ View(koi_data)
 
 library(ggplot2)
 
-ggplot(data = koi_data, aes(x = koi_disposition, y = koi_disposition)) +
+ggplot(data = koi_data, aes(x = koi_disposition, fill = koi_disposition)) +
   labs (title = "Exoplanet Disposition") +
-  geom_bar(stat = "summary", fun = "mean")
-
-ggplot(data = koi_data, aes(x = koi_srad)) +
-  labs(title = "Stellar Radius") +
-  geom_density()
-
-ggplot(data = koi_data, aes(x = koi_prad)) +
-  labs(title = "Exoplanet Radius") +
-  geom_density()
+  geom_bar()
